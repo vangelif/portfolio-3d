0d8f749f6834cc5adab316f0017128b9c697bea6
Vangelis <ev.fotiades@yahoo.com>
2024-02-08 18:39:28 +0200

    refactor balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,9 +9,9 @@ const Ball = (props) => {
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
+      <directionalLight position={[0, 0, 0.075]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]} />
+        <icosahedronGeometry args={[1,3]} />
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
         <Decal position={[0, 0, 1]} rotation={[ 2 * Math.PI, 0, 6.25 ]} flatShading map={decal} />
       </mesh>
