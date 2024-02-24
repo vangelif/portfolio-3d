41a6b3b03188fe1a60758d6c6037f54bd80a0a2c
Vangelis <ev.fotiades@yahoo.com>
2024-02-24 14:51:13 +0200

    refactore file structure of png file

--- src/assets/index.js
+++ src/assets/index.js
@@ -38,7 +38,7 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-import countries from "../assets/countries.png"
+import countries from "./countries.png";
 
 export {
   vologo,
